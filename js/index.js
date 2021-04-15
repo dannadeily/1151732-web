b3c07ec0723caae3760ebbadf55e3a6a3fa425e6
dannadeily <dannadeilydc@ufps.edu.co>
2021-04-15 07:51:13 -0500

    tercer commit

--- js/index.js
+++ js/index.js
@@ -72,3 +72,29 @@ const indexHours = (id) => {
     let date = new Date();
     parent.append(`${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`);
 }
+
+const indexGenericDinamic  = (url, id) =>{
+    let getUrlValue = getParameterByName("id");
+    url = `${url}/${getUrlValue}`;
+    console.log(url);
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let info = JSON.parse(this.responseText);
+            let parent = document.getElementById(id);
+            let fragment = new DocumentFragment();
+            let h1 = createElement("H1", "titulo", `${info.titulo} - ${info.categoria} - ${info.fecha}`);
+            let div = createElement("DIV", "container-index", h1);
+            let des = createElement("P", null, info.descripcion);
+            let det = createElement("P", null, info.detalle);
+            let link = createElement("A", null, "Ver mas");
+            div.append(des);
+            des.append(link);
+            div.append(det);
+            fragment.append(div);
+            parent.append(fragment);
+        }
+    }
+}
